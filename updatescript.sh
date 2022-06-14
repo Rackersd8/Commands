cca72408aa0f0e86f93576f35e5c264f84c0ee35
Rackersd8 <89323117+Rackersd8@users.noreply.github.com>
2022-06-14 16:29:23 +0000

    Add files via upload

---
+++ updatescript.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "Making this script executable...."
+
+sleep 5s
+
+sudo chmod u+x updatescript.sh
+
+sleep 5s
+
+echo "This script is now executable...."
+
+sleep 5s 
+
+echo "Checking for nano text editor...."
+
+sleep 5s
+
+if [ $(dpkg-query -W -f='${Status}' nano 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then 
+apt-get install nano;
+fi
+
+sleep 10s  
+
+echo "Checking for system update...."
+
+sleep 10s
+
+sudo apt update
+
+sleep 10s 
+
+echo "Checking for system upgrade...."
+
+sleep 10s
+
+yes | sudo apt upgrade
+
+echo "Update Successful! All Done!"
+
+
